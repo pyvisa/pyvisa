6c0d79f0fc1236bc3eac7b4ba0d0f03feb44af73
bronger <bronger@516c0664-e049-0410-bc6e-8f9a6051a51d>
2005-03-16 19:33:14 +0000

    Added some meta data.  Made ctypes import in a way that it's not re-exported again.
    git-svn-id: svn://svn.code.sf.net/p/pyvisa/code/trunk@14 516c0664-e049-0410-bc6e-8f9a6051a51d

--- vpp43_types.py
+++ vpp43_types.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 #
 #    vpp43_types.py - VISA VPP-4.3 data types (VPP-4.3.2 spec, section 3)
-#	$Id$	
 #
 #    Copyright © 2005 Gregor Thalhammer <gth@users.sourceforge.net>,
 #                     Torsten Bronger <bronger@physik.rwth-aachen.de>.
@@ -24,7 +23,17 @@
 #    Temple Place, Suite 330, Boston, MA 02111-1307 USA
 #
 
-from ctypes import *
+"""All data types that are defined by VPP-4.3.2, including the pointer and
+array types.  This means "ViUInt32" and such.
+"""
+
+__version__ = "$Revision$"
+# $Source$
+
+
+# ctypes shouldn't be re-exported.
+import ctypes as _ctypes
+
 
 # Part One: Type Assignments for VISA and Instrument Drivers, see spec table
 # 3.1.1.
@@ -34,38 +43,38 @@ from ctypes import *
 # However, as long as I'm not fully sure about this, they won't hurt.
 
 def _type_dublet(ctypes_type):
-    return (ctypes_type, POINTER(ctypes_type))
+    return (ctypes_type, _ctypes.POINTER(ctypes_type))
 
 def _type_triplet(ctypes_type):
-    return _type_dublet(ctypes_type) + (POINTER(ctypes_type),)
+    return _type_dublet(ctypes_type) + (_ctypes.POINTER(ctypes_type),)
 
-ViUInt32, ViPUInt32, ViAUInt32    = _type_triplet(c_ulong)
-ViInt32, ViPInt32, ViAInt32       = _type_triplet(c_long)
-ViUInt16, ViPUInt16, ViAUInt16    = _type_triplet(c_ushort)
-ViInt16, ViPInt16, ViAInt16       = _type_triplet(c_short)
-ViUInt8, ViPUInt8, ViAUInt8       = _type_triplet(c_ubyte)
-ViInt8, ViPInt8, ViAInt8          = _type_triplet(c_byte)
-ViAddr, ViPAddr, ViAAddr          = _type_triplet(c_void_p)
-ViChar, ViPChar, ViAChar          = _type_triplet(c_char)
-ViByte, ViPByte, ViAByte          = _type_triplet(c_ubyte)
+ViUInt32, ViPUInt32, ViAUInt32    = _type_triplet(_ctypes.c_ulong)
+ViInt32, ViPInt32, ViAInt32       = _type_triplet(_ctypes.c_long)
+ViUInt16, ViPUInt16, ViAUInt16    = _type_triplet(_ctypes.c_ushort)
+ViInt16, ViPInt16, ViAInt16       = _type_triplet(_ctypes.c_short)
+ViUInt8, ViPUInt8, ViAUInt8       = _type_triplet(_ctypes.c_ubyte)
+ViInt8, ViPInt8, ViAInt8          = _type_triplet(_ctypes.c_byte)
+ViAddr, ViPAddr, ViAAddr          = _type_triplet(_ctypes.c_void_p)
+ViChar, ViPChar, ViAChar          = _type_triplet(_ctypes.c_char)
+ViByte, ViPByte, ViAByte          = _type_triplet(_ctypes.c_ubyte)
 ViBoolean, ViPBoolean, ViABoolean = _type_triplet(ViUInt16)
-ViReal32, ViPReal32, ViAReal32    = _type_triplet(c_float)
-ViReal64, ViPReal64, ViAReal64    = _type_triplet(c_double)
+ViReal32, ViPReal32, ViAReal32    = _type_triplet(_ctypes.c_float)
+ViReal64, ViPReal64, ViAReal64    = _type_triplet(_ctypes.c_double)
 
 # The following three type triplets are defined rather pathologically, both in
 # the spec and the reference .h file.  Therefore, I can't use _type_triplet.
 
 ViBuf         = ViPByte
 ViPBuf        = ViPByte
-ViABuf        = POINTER(ViBuf)
+ViABuf        = _ctypes.POINTER(ViBuf)
 
 ViString      = ViPChar
 ViPString     = ViPChar
-ViAString     = POINTER(ViString)
+ViAString     = _ctypes.POINTER(ViString)
 
 ViRsrc        = ViString
 ViPRsrc       = ViString
-ViARsrc       = POINTER(ViRsrc)
+ViARsrc       = _ctypes.POINTER(ViRsrc)
 
 ViStatus, ViPStatus, ViAStatus    = _type_triplet(ViInt32)
 ViVersion, ViPVersion, ViAVersion = _type_triplet(ViUInt32)
@@ -73,7 +82,7 @@ ViObject, ViPObject, ViAObject    = _type_triplet(ViUInt32)
 ViSession, ViPSession, ViASession = _type_triplet(ViObject)
 
 ViAttr        = ViUInt32
-ViConstString = POINTER(ViChar)
+ViConstString = _ctypes.POINTER(ViChar)
 
 
 # Part Two: Type Assignments for VISA only, see spec table 3.1.2.  The
@@ -88,11 +97,11 @@ ViBusSize     = ViUInt32
 ViAttrState, ViPAttrState   = _type_dublet(ViUInt32)
 
 # The following is weird, taken from news:zn2ek2w2.fsf@python.net
-ViVAList      = POINTER(c_char)
+ViVAList      = _ctypes.POINTER(_ctypes.c_char)
 
 ViEventType, ViPEventType, ViAEventType = _type_triplet(ViUInt32)
 
-ViPAttr       = POINTER(ViAttr)
+ViPAttr       = _ctypes.POINTER(ViAttr)
 ViAAttr       = ViPAttr
 
 ViEventFilter = ViUInt32
@@ -102,4 +111,4 @@ ViEvent, ViPEvent           = _type_dublet(ViObject)
 ViKeyId, ViPKeyId           = _type_dublet(ViString)
 ViJobId, ViPJobId           = _type_dublet(ViUInt32)
 
-ViHndlr       = CFUNCTYPE(ViSession, ViEventType, ViEvent, ViAddr)
+ViHndlr       = _ctypes.CFUNCTYPE(ViSession, ViEventType, ViEvent, ViAddr)
