4323f342d1bb128c57abb149c87cde6d93268f63
bronger <bronger@516c0664-e049-0410-bc6e-8f9a6051a51d>
2005-03-19 13:00:35 +0000

    Added some documentation.
    git-svn-id: svn://svn.code.sf.net/p/pyvisa/code/trunk@30 516c0664-e049-0410-bc6e-8f9a6051a51d

--- vpp43.py
+++ vpp43.py
@@ -23,6 +23,12 @@
 #    Temple Place, Suite 330, Boston, MA 02111-1307 USA
 #
 
+"""Main module of the implementation of the original VISA routines.
+
+See VPP-4.3.2.
+
+"""
+
 __version__ = "$Revision$"
 # $Source$
 
@@ -35,19 +41,13 @@ from visa_exceptions import *
 import os
 
 
-def check_status(status):
-    """Check return values for errors."""
-    if status < 0:
-        raise VisaIOError, status
-    else:
-        return status
-
-
 # load VISA library
 
 class _Singleton(object):
-    """Base class for singleton classes.  Taken from
-    <http://www.python.org/2.2.3/descrintro.html>.
+    """Base class for singleton classes.
+
+    Taken from <http://www.python.org/2.2.3/descrintro.html>.
+
     """
     def __new__(cls, *args, **kwds):
         it = cls.__dict__.get("__it__")
@@ -60,9 +60,29 @@ class _Singleton(object):
         pass
 
 class _VisaLibrary(_Singleton):
+    """Singleton class for VISA ctypes library handle.
+
+    This class has only one instance called "visa_library".  The purpose of its
+    instance is to provide access to the ctypes object that contains the VISA
+    library.
+
+    Public methods:
+    load_library -- (re-)loads the VISA library
+    __call__     -- returns the ctypes object holding the VISA library
+    """
     def init(self):
 	self.__lib = None
     def load_library(self, path = "/usr/local/vxipnp/linux/bin/libvisa.so.7"):
+	"""(Re-)loads the VISA library.
+
+	The optional parameter "path" holds the full path to the VISA library.
+	At the moment, it has significance only for Linux.  It is called
+	implicitly by __call__ if not called successfully before.
+
+	It may raise an OSNotSupported exception, or an OSError if the library
+	file was not found.
+
+	"""
 	if os.name == 'nt':
 	    self.__lib = windll.visa32
 	elif os.name == 'posix':
@@ -71,9 +91,18 @@ class _VisaLibrary(_Singleton):
 	    self.__lib = None
 	    raise OSNotSupported, os.name
     def __call__(self):
+	"""Returns the ctypes object to the VISA library."""
 	if self.__lib is None:
 	    self.load_library()
 	return self.__lib
 
 visa_library = _VisaLibrary()
+
+
+def check_status(status):
+    """Check return values for errors."""
+    if status < 0:
+        raise VisaIOError, status
+    else:
+        return status
 
