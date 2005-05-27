e2fa21823eedb34ef31f23b525a1669b4b80ff2a
bronger <bronger@516c0664-e049-0410-bc6e-8f9a6051a51d>
2005-05-27 13:08:57 +0000

    Added UnknownHandler.  Renamed VisaTypeError to TypeError.
    git-svn-id: svn://svn.code.sf.net/p/pyvisa/code/trunk@90 516c0664-e049-0410-bc6e-8f9a6051a51d

--- visa_exceptions.py
+++ visa_exceptions.py
@@ -48,16 +48,31 @@ class VisaIOError(Error):
 	Error.__init__(self, abbreviation + ": " + description)
 	self.error_code = error_code
 
-class VisaTypeError(Error):
+class TypeError(Error):
     """Exception class for wrong types in VISA function argument lists.
 
     Raised if unsupported types are given to scanf, sscanf, printf, sprintf,
-    and queryf.  Because the current implementation doesn't analyse the farmat
+    and queryf.  Because the current implementation doesn't analyse the format
     strings, it can only deal with integers, floats, and strings.
+
+    Additionally, this exception is raised by install_handler if un unsupported
+    type is used for the user handle.
 
     """
     def __init__(self, description):
 	Error.__init__(self, description)
+
+class UnknownHandler(Error):
+    """Exception class for invalid handler data given to uninstall_handler().
+
+    uninstall_handler() checks whether the handler and user_data parameters
+    point to a known handler previously installed with install_handler().  If
+    it can't find it, this exception is raised.
+
+    """
+    def __init__(self):
+	Error.__init__(self, "Handler with this handler function and user data"
+		       " not found")
 
 class OSNotSupported(Error):
     def __init__(self, os):
