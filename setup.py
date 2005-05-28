a168b6d2f5ed20ad7babace91bd1b7fd33a81797
bronger <bronger@516c0664-e049-0410-bc6e-8f9a6051a51d>
2005-05-28 19:38:40 +0000

    Ordinary update.
    git-svn-id: svn://svn.code.sf.net/p/pyvisa/code/trunk@94 516c0664-e049-0410-bc6e-8f9a6051a51d

--- setup.py
+++ setup.py
@@ -3,22 +3,26 @@
 from distutils.core import setup
 
 classifiers = [
-    'Development Status :: 1 - Planning',
+    'Development Status :: 3 - Alpha',
     'Intended Audience :: Developers',
     'Intended Audience :: Science/Research',
     'License :: OSI Approved :: GNU General Public License (GPL)',
     'Operating System :: Microsoft :: Windows',
-    'Operating System :: POSIX',
+    'Operating System :: Linux',
     'Programming Language :: Python',
     'Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator',
     'Topic :: Software Development :: Libraries :: Python Modules']
 
 setup(name = 'pyvisa',
-      description = 'pyvisa, a python package to support the VISA I/O standard',
+      description = 'PyVISA, a python package to support the VISA I/O standard',
       version = '0.1',
-      long_description = 'visa, a package to support the VISA I/O standard',
+      long_description = 'A Python package for support of the "Virtual '\
+          'Instrument Software Architecture" (VISA), in order to control '\
+          'measurement devices and test equipment via GPIB, RS232, or USB.',
       author = 'Gregor Thalhammer, Torsten Bronger',
       author_email = 'Gregor.Thalhammer@uibk.ac.at',
       url = 'http://sourceforge.net/projects/pyvisa',
       classifiers = classifiers,
-      py_modules = ['visa', 'visa_messages', 'visa_attributes', 'vpp43_types', 'vpp43_constants'])
+      py_modules = ['visa', 'visa_messages', 'visa_attributes', 'vpp43',
+		    'vpp43_attributes', 'visa_exceptions', 'vpp43_types',
+		    'vpp43_constants'])
