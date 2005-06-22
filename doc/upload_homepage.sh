66f0fe11ae65fcb2a05bb9eacb7bcb3c10107b40
bronger <bronger@516c0664-e049-0410-bc6e-8f9a6051a51d>
2005-06-22 18:13:54 +0000

    Added PDF version.
    git-svn-id: svn://svn.code.sf.net/p/pyvisa/code/trunk@160 516c0664-e049-0410-bc6e-8f9a6051a51d

--- doc/upload_homepage.sh
+++ doc/upload_homepage.sh
@@ -3,7 +3,7 @@
 rst2html.py vpp43.txt > vpp43.html
 rst2html.py homepage.txt > index.html
 rm -Rf pyvisa
-mkhowto --html pyvisa.tex
+mkhowto --html --pdf --up-link http://pyvisa.sourceforge.net --up-title "PyVISA homepage" pyvisa.tex
 
-scp vpp43.html index.html default.css shell.sf.net:/home/groups/p/py/pyvisa/htdocs/
+scp vpp43.html index.html default.css pyvisa.pdf shell.sf.net:/home/groups/p/py/pyvisa/htdocs/
 scp -r pyvisa/* shell.sf.net:/home/groups/p/py/pyvisa/htdocs/pyvisa/
