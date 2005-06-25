96488aaeb8561151104f529353f055910f08161e
bronger <bronger@516c0664-e049-0410-bc6e-8f9a6051a51d>
2005-06-25 21:32:37 +0000

    New file.
    git-svn-id: svn://svn.code.sf.net/p/pyvisa/code/trunk@175 516c0664-e049-0410-bc6e-8f9a6051a51d

---
+++ test/test_keithley2000.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#    test_keithley2000.py - PyVISA test code for Keithley 2000 multimeter
+#
+#    Copyright © 2005 Gregor Thalhammer <gth@users.sourceforge.net>,
+#		      Torsten Bronger <bronger@physik.rwth-aachen.de>.
+#
+#    This file is part of PyVISA.
+#
+#    PyVISA is free software; you can redistribute it and/or modify it under
+#    the terms of the GNU General Public License as published by the Free
+#    Software Foundation; either version 2 of the License, or (at your option)
+#    any later version.
+#
+#    pyvisa is distributed in the hope that it will be useful, but WITHOUT ANY
+#    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+#    FOR A PARTICULAR PURPOSE.	See the GNU General Public License for more
+#    details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with pyvisa; if not, write to the Free Software Foundation, Inc., 59
+#    Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
+
+from visa import *
+
+print "Test start"
+keithley = GpibInstrument(12)
+milliseconds = 500
+number_of_values = 10
+keithley.write("F0B2M2G0T2Q%dI%dX" % (milliseconds, number_of_values))
+keithley.trigger()
+keithley.wait_for_srq()
+print keithley.read_floats()
+print "Average: ", sum(voltages) / len(voltages)
+print "Test end"
