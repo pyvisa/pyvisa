152baeea27eabc120116e60f2e6f4681370edf5b
bronger <bronger@516c0664-e049-0410-bc6e-8f9a6051a51d>
2005-06-26 20:12:02 +0000

    File deleted.
    git-svn-id: svn://svn.code.sf.net/p/pyvisa/code/trunk@187 516c0664-e049-0410-bc6e-8f9a6051a51d

--- doc/upload_homepage.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-rst2html.py vpp43.txt > vpp43.html
-rst2html.py homepage.txt > index.html
-rm -Rf pyvisa
-mkhowto --html --pdf --up-link http://pyvisa.sourceforge.net --up-title "PyVISA homepage" pyvisa.tex
-
-scp vpp43.html index.html default.css pyvisa.pdf shell.sf.net:/home/groups/p/py/pyvisa/htdocs/
-scp -r pyvisa/* shell.sf.net:/home/groups/p/py/pyvisa/htdocs/pyvisa/
