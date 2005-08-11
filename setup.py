7576176213ebf343c565603dc364e4f8676d4181
bronger <bronger@516c0664-e049-0410-bc6e-8f9a6051a51d>
2005-08-11 11:33:41 +0000

    Changed package name to "PyVISA".  Changed version number to 0.9.5.
    git-svn-id: svn://svn.code.sf.net/p/pyvisa/code/trunk@262 516c0664-e049-0410-bc6e-8f9a6051a51d

--- setup.py
+++ setup.py
@@ -73,9 +73,9 @@ if os.name == 'posix':
 
 # The release name must be changed here and in doc/pyvisa.tex
 
-setup(name = 'pyvisa',
+setup(name = 'PyVISA',
       description = 'Python VISA bindings for GPIB, RS232, and USB instruments',
-      version = '0.9.4',
+      version = '0.9.5',
       long_description = \
       """A Python package for support of the 'Virtual Instrument Software
 Architecture' (VISA), in order to control measurement devices and test
