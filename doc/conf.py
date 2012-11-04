9c626921029d1b8eddbeaee008bcc398290dfc46
bauflo3 <bauflo3@516c0664-e049-0410-bc6e-8f9a6051a51d>
2012-11-04 16:10:45 +0000

    correct e-mail address in documentation
    Also added a more prominently placed download link and reformatted a
    couple of the paragraphs in the source file.
    
    git-svn-id: svn://svn.code.sf.net/p/pyvisa/code/trunk@371 516c0664-e049-0410-bc6e-8f9a6051a51d

--- doc/conf.py
+++ doc/conf.py
@@ -30,7 +30,7 @@ templates_path = ['_templates']
 source_suffix = '.txt'
 
 # The encoding of source files.
-#source_encoding = 'utf-8'
+source_encoding = 'latin-1'
 
 # The master toctree document.
 master_doc = 'pyvisa'
