d04f408edf2ce854a0a2f71dc6f616a80be94187
Hernan Grecco <hernan.grecco@gmail.com>
2013-08-24 21:15:44 -0300

    Improved setup

--- setup.py
+++ setup.py
@@ -31,44 +31,45 @@
 #    DEALINGS IN THE SOFTWARE.
 #
 
-from distribute_setup import use_setuptools
-use_setuptools("0.6.24")
-from setuptools import setup
-from setuptools import find_packages
-
-import distutils.dir_util
-import shutil, os.path, atexit
-import sys
-
 try:
-    distutils.dir_util.remove_tree("build")
+    import sys
+    reload(sys).setdefaultencoding("UTF-8")
 except:
     pass
 
-extra = {}
-if sys.version_info >= (3,):
-    extra['use_2to3'] = True
-# The release name must be changed here and in doc/pyvisa.tex
 
-setup(name = 'PyVISA',
-      description = 'Python VISA bindings for GPIB, RS232, and USB instruments',
-      version = '1.5',
-      long_description = \
-      """A Python package for support of the Virtual Instrument
-Software Architecture (VISA), in order to control measurement devices
-and test equipment via GPIB, RS232, or USB.
+from distribute_setup import use_setuptools
+use_setuptools()
 
-Homepage: http://pyvisa.sourceforge.net
-""",
-      author = 'Torsten Bronger, Gregor Thalhammer',
-      author_email = 'bronger@physik.rwth-aachen.de',
-      maintainer = 'Florian Bauer',
-      maintainer_email = 'pyvisa-devel@lists.sourceforge.net',
-      url = 'http://pyvisa.sourceforge.net',
-      download_url = 'http://sourceforge.net/projects/pyvisa/',
-      keywords = 'VISA GPIB USB serial RS232 measurement acquisition',
-      license = 'MIT License',
-      classifiers = [
+from setuptools import setup, find_packages
+
+import codecs
+
+
+def read(filename):
+    return codecs.open(filename, encoding='utf-8').read()
+
+
+long_description = '\n\n'.join([read('README'),
+                                read('AUTHORS'),
+                                read('CHANGES')])
+
+__doc__ = long_description
+
+setup(name='PyVISA',
+      description='Python VISA bindings for GPIB, RS232, and USB instruments',
+      version='1.5.dev0',
+      long_description=long_description,
+      author='Torsten Bronger, Gregor Thalhammer',
+      author_email='bronger@physik.rwth-aachen.de',
+      maintainer='Florian Bauer',
+      maintainer_email='pyvisa-devel@lists.sourceforge.net',
+      url='http://pyvisa.sourceforge.net',
+      download_url='http://sourceforge.net/projects/pyvisa/',
+      keywords='VISA GPIB USB serial RS232 measurement acquisition',
+      license='MIT License',
+      test_suite='visa.testsuite.testsuite',
+      classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
@@ -78,9 +79,12 @@ Homepage: http://pyvisa.sourceforge.net
         'Programming Language :: Python',
         'Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator',
         'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
         ],
-      packages = find_packages(),
-      platforms = "Linux, Windows",
-      py_modules = ['visa'],
-      setup_requires = ["sphinx>=1.0", "Mock"],
-      **extra)
+      packages=find_packages(),
+      platforms="Linux, Windows",
+      py_modules=['visa'],
+      setup_requires=["sphinx>=1.0", "Mock"],
+      use_2to3=True,
+      zip_safe=False)
