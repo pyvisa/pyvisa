276ad93885b7ae727abe9e6a0ae52b78d38c38a2
Hernan Grecco <hernan.grecco@gmail.com>
2014-02-12 01:58:50 -0300

    Moved visa.py outside the package.

--- visa.py
+++ visa.py
@@ -16,3 +16,5 @@
 from __future__ import division, unicode_literals, print_function, absolute_import
 
 from pyvisa.highlevel import VisaLibrary, ResourceManager, get_instruments_list, instrument
+from pyvisa.errors import (Error, VisaIOError, VisaIOWarning, VisaTypeError,
+                           UnknownHandler, OSNotSupported, InvalidBinaryFormat)
