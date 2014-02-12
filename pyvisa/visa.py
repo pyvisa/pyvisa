d6b72600690cf2efc592c15d79948689ca5543d7
Hernan Grecco <hernan.grecco@gmail.com>
2014-02-12 01:29:55 -0300

    Added visa module, which is installed by setup.py
    This module allows the user to do:
    
       >>> import visa
    
    instead of:
    
       >>> import pyvisa
    
    Is kept for backwards compatibility reasons.

---
+++ pyvisa/visa.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+    pyvisa.visa
+    ~~~~~~~~~~~
+
+    Module to provide an import shortcut for the most common VISA operations.
+
+    This is a legacy module for backwards compatibility with PyVISA < 1.5
+
+    This file is part of PyVISA.
+
+    :copyright: (c) 2014 by the PyVISA authors.
+    :license: MIT, see COPYING for more details.
+"""
+
+from __future__ import division, unicode_literals, print_function, absolute_import
+
+from pyvisa.highlevel import VisaLibrary, ResourceManager, get_instruments_list, instrument
