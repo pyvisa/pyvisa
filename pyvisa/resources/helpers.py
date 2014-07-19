900c5abfaebb14f8292dadb55156b04ee8114385
Hernan Grecco <hernan.grecco@gmail.com>
2014-07-19 02:59:44 -0300

    Implemented read-only version of properties.

--- pyvisa/resources/helpers.py
+++ pyvisa/resources/helpers.py
@@ -1,10 +1,43 @@
 __author__ = 'grecco'
 
 
-def range_property(attribute_name, min_value, max_value, doc=None, transform=None):
+def attr_property(attribute_name, doc=None, ro=False):
+    def getter(self):
+        return self.get_visa_attribute(attribute_name)
+
+    if ro:
+        return property(fget=getter, doc=doc)
+
+    def setter(self, value):
+        self.set_visa_attribute(attribute_name, value)
+
+    return property(fget=getter, fset=setter, doc=doc)
+
+
+def enum_property(attribute_name, enum_type, doc=None, ro=False):
+
+    def getter(self):
+        return enum_type(self.get_visa_attribute(attribute_name))
+
+    if ro:
+        return property(fget=getter, doc=doc)
+
+    def setter(self, value):
+        if value not in enum_type:
+            raise ValueError('%r is an invalid value for attribute %s, should be a %r',
+                             value, attribute_name, enum_type)
+        self.set_visa_attribute(attribute_name, value)
+
+    return property(fget=getter, fset=setter, doc=doc)
+
+
+def range_property(attribute_name, min_value, max_value, doc=None, transform=None, ro=False):
 
     def getter(self):
         return self.get_visa_attribute(attribute_name)
+
+    if ro:
+        return property(fget=getter, doc=doc)
 
     def setter(self, value):
         if not min_value <= value <= max_value:
@@ -15,10 +48,13 @@ def range_property(attribute_name, min_value, max_value, doc=None, transform=Non
     return property(fget=getter, fset=setter, doc=doc)
 
 
-def boolean_property(attribute_name, doc=doc):
+def boolean_property(attribute_name, doc=doc, ro=False):
 
     def getter(self):
         return self.get_visa_attribute(attribute_name) == VI_TRUE
+
+    if ro:
+        return property(fget=getter, doc=doc)
 
     def setter(self, value):
         self.set_visa_attribute(attribute_name, VI_TRUE if value else VI_FALSE)
@@ -26,10 +62,13 @@ def boolean_property(attribute_name, doc=doc):
     return property(fget=getter, fset=setter, doc=doc)
 
 
-def char_property(attribute_name, doc=doc):
+def char_property(attribute_name, doc=doc, ro=False):
 
     def getter(self):
         return chr(self.get_visa_attribute(attribute_name))
+
+    if ro:
+        return property(fget=getter, doc=doc)
 
     def setter(self, value):
         self.set_visa_attribute(attribute_name, ord(value))
