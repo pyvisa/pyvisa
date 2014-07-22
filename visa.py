94ece0c8cc33484dfe031ba7378ffff76e38361d
Hernan Grecco <hernan.grecco@gmail.com>
2014-07-22 18:50:20 -0300

    Removed imports of old functions

--- visa.py
+++ visa.py
@@ -16,6 +16,6 @@
 from __future__ import division, unicode_literals, print_function, absolute_import
 
 from pyvisa import logger, __version__, log_to_screen
-from pyvisa.highlevel import VisaLibrary, ResourceManager, get_instruments_list, instrument
+from pyvisa.highlevel import VisaLibrary, ResourceManager
 from pyvisa.errors import (Error, VisaIOError, VisaIOWarning, VisaTypeError,
                            UnknownHandler, OSNotSupported, InvalidBinaryFormat)
