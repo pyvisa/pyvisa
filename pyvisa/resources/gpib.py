0cec8c4992f731b0f0a0f4693ea60495a92ede42
Hernan Grecco <hernan.grecco@gmail.com>
2014-07-22 03:12:17 -0300

    Improved gpib classes

--- pyvisa/resources/gpib.py
+++ pyvisa/resources/gpib.py
@@ -3,7 +3,7 @@
     pyvisa.resources.gpib
     ~~~~~~~~~~~~~~~~~~~~~
 
-    High level wrapper for Serial resources.
+    High level wrapper for GPIB resources.
 
     This file is part of PyVISA.
 
@@ -11,126 +11,201 @@
     :license: MIT, see LICENSE for more details.
 """
 
+from __future__ import division, unicode_literals, print_function, absolute_import
+
 import time
-import warnings
-import contextlib
-from enum import IntEnum
 
-from .. import logger
-from ..constants import *
-from .. import errors
-from ..util import (warning_context, split_kwargs, warn_for_invalid_kwargs,
-                    parse_ascii, parse_binary)
-
+from .. import constants
+from .resource import Resource
 from .messagebased import MessageBasedResource
-from .helpers import range_property
+from . import helpers as hlp
 
 
-class GpibResource(MessageBasedResource):
-    """Class for GPIB Resources.
-
-    This class extents the Instrument class with special operations and
-    properties of GPIB instruments.
-
-    :param gpib_identifier: strings are interpreted as instrument's VISA resource name.
-                            Numbers are interpreted as GPIB number.
-    :param board_number: the number of the GPIB bus.
-
-    Further keyword arguments are passed to the constructor of class
-    Instrument.
-
+class _GPIBMixin(object):
+    """Common attributes and methods of GPIB Instr and Interface.
     """
 
-    def __init__(self, gpib_identifier, board_number=0, resource_manager=None, **keyw):
-        warn_for_invalid_kwargs(keyw, MessageBasedInstrument.ALL_KWARGS.keys())
-        if isinstance(gpib_identifier, int):
-            resource_name = "GPIB%d::%d" % (board_number, gpib_identifier)
-        else:
-            resource_name = gpib_identifier
+    visalib = None
+    session = None
 
-        super(GpibInstrument, self).__init__(resource_name, resource_manager, **keyw)
-
-        # Now check whether the instrument is really valid
-        if self.interface_type != VI_INTF_GPIB:
-            raise ValueError("device is not a GPIB instrument")
-
-        self.visalib.enable_event(self.session, VI_EVENT_SERVICE_REQ, VI_QUEUE)
-
-    def __del__(self):
-        if self.session is not None:
-            self.__switch_events_off()
-            super(GpibInstrument, self).__del__()
-
-    def __switch_events_off(self):
-        self.visalib.disable_event(self.session, VI_ALL_ENABLED_EVENTS, VI_ALL_MECH)
-        self.visalib.discard_events(self.session, VI_ALL_ENABLED_EVENTS, VI_ALL_MECH)
-
-    allow_dma = boolean_property(VI_ATTR_DMA_ALLOW_EN,
+    allow_dma = hlp.boolean_attr('VI_ATTR_DMA_ALLOW_EN',
                                  doc='This attribute specifies whether I/O accesses should '
                                      'use DMA (True) or Programmed I/O (False).')
 
-    primary_address = attr_property(VI_ATTR_GPIB_PRIMARY_ADDR,
-                                    doc='Specifies the primary address of the GPIB device used by the given session.',
-                                    ro=True)
+    primary_address = hlp.attr('VI_ATTR_GPIB_PRIMARY_ADDR',
+                               doc='Specifies the primary address of the GPIB device used by the given session.',
+                               ro=True)
 
-    enable_repeat_addressing = boolean_property(VI_ATTR_GPIB_READDR_EN,
-                                                doc='Specifies whether to use repeat addressing '
-                                                    'before each read or write operation.')
-
-    remote_enabled = enum_property(VI_ATTR_GPIB_REN_STATE, contants.LineState,
+    remote_enabled = hlp.enum_attr('VI_ATTR_GPIB_REN_STATE', constants.LineState,
                                    doc='Returns the current state of the GPIB REN (Remote ENable) interface line.',
                                    ro=True)
 
-    primary_address = attr_property(VI_ATTR_GPIB_SECONDARY_ADDR,
-                                    doc='Specifies the secondary address of the GPIB device used by the given session.',
-                                    ro=True)
+    secondary_address = hlp.attr('VI_ATTR_GPIB_SECONDARY_ADDR',
+                                 doc='Specifies the secondary address of the GPIB device used by the given session.',
+                                 ro=True)
 
-    enable_unaddressing = boolean_property(VI_ATTR_GPIB_UNADDR_EN,
+    def assert_trigger(self):
+        """Sends a software trigger to the device.
+        """
+
+        self.visalib.assert_trigger(self.session, constants.VI_TRIG_PROT_DEFAULT)
+
+    def send_command(self, data):
+        """Write GPIB command bytes on the bus.
+
+        Corresponds to viGpibCommand function of the VISA library.
+
+        :param data: data tor write.
+        :type data: bytes
+        :return: Number of written bytes, return value of the library call.
+        :rtype: int, VISAStatus
+        """
+        return self.visalib.viGpibCommand(self.session, data, data)
+
+    def control_atn(self, mode):
+        """Specifies the state of the ATN line and the local active controller state.
+
+        Corresponds to viGpibControlATN function of the VISA library.
+
+        :param mode: Specifies the state of the ATN line and optionally the local active controller state.
+                     (Constants.GPIB_ATN*)
+        :return: return value of the library call.
+        :rtype: VISAStatus
+        """
+        return self.visalib.viGpibControlATN(self.session, mode)
+
+    def control_ren(self, mode):
+        """Controls the state of the GPIB Remote Enable (REN) interface line, and optionally the remote/local
+        state of the device.
+
+        Corresponds to viGpibControlREN function of the VISA library.
+
+        :param mode: Specifies the state of the REN line and optionally the device remote/local state.
+                     (Constants.GPIB_REN*)
+        :return: return value of the library call.
+        :rtype: VISAStatus
+        """
+        return self.visalib.viGpibControlREN(self.session, mode)
+
+    def pass_control(self, primary_address, secondary_address):
+        """Tell the GPIB device at the specified address to become controller in charge (CIC).
+
+        Corresponds to viGpibPassControl function of the VISA library.
+
+        :param primary_address: Primary address of the GPIB device to which you want to pass control.
+        :param secondary_address: Secondary address of the targeted GPIB device.
+                                  If the targeted device does not have a secondary address,
+                                  this parameter should contain the value Constants.NO_SEC_ADDR.
+        :return: return value of the library call.
+        :rtype: VISAStatus
+        """
+        return self.visalib.viGpibPassControl(self.session, primary_address, secondary_address)
+
+    def send_ifc(self):
+        """Pulse the interface clear line (IFC) for at least 100 microseconds.
+
+        Corresponds to viGpibSendIFC function of the VISA library.
+
+        :return: return value of the library call.
+        :rtype: VISAStatus
+        """
+        return self.visalib.viGpibSendIFC(self.session)
+
+
+@Resource.register(constants.InterfaceType.gpib, 'INSTR')
+class GPIBInstrument(_GPIBMixin, MessageBasedResource):
+    """Communicates with to devices of type GPIB::<primary address>[::INSTR]
+
+    More complex resource names can be specified with the following grammar:
+        GPIB[board]::primary address[::secondary address][::INSTR]
+
+    Do not instantiate directly, use :meth:`pyvisa.highlevel.ResourceManager.open`.
+
+    :param resource_manager: A resource manager instance.
+    :param resource_name: the VISA name for the resource.
+    """
+
+    def before_close(self):
+        self.__switch_events_off()
+        super(GPIBInstrument, self).before_close()
+
+    def __switch_events_off(self):
+        self.visalib.disable_event(self.session, constants.VI_ALL_ENABLED_EVENTS, constants.VI_ALL_MECH)
+        self.visalib.discard_events(self.session, constants.VI_ALL_ENABLED_EVENTS, constants.VI_ALL_MECH)
+
+    enable_repeat_addressing = hlp.boolean_attr('VI_ATTR_GPIB_READDR_EN',
+                                                doc='Specifies whether to use repeat addressing '
+                                                    'before each read or write operation.')
+
+    enable_unaddressing = hlp.boolean_attr('VI_ATTR_GPIB_UNADDR_EN',
                                            doc='Specifies whether to unaddress the device (UNT and UNL) '
                                                'after each read or write operation.')
 
-    def remote_mode(self, mode):
-        """Controls the state of the GPIB Remote Enable (REN) interface line,
-        and optionally the remote/local state of the device.
-
-        :param mode: Specifies the state of the REN line and optionally
-                     the device remote/local state. See values VI_GPIB_REN_*
-        """
-        self._visalib.gpib_control_ren(mode)
-
-    def wait_for_srq(self, timeout=25):
+    def wait_for_srq(self, timeout=25000):
         """Wait for a serial request (SRQ) coming from the instrument.
 
         Note that this method is not ended when *another* instrument signals an
         SRQ, only *this* instrument.
 
-        :param timeout: the maximum waiting time in seconds.
-                        Defaul: 25 (seconds).
+        :param timeout: the maximum waiting time in milliseconds.
+                        Defaul: 25000 (seconds).
                         None means waiting forever if necessary.
         """
         lib = self.visalib
 
-        lib.enable_event(self.session, VI_EVENT_SERVICE_REQ, VI_QUEUE)
+        lib.enable_event(self.session, constants.VI_EVENT_SERVICE_REQ, constants.VI_QUEUE)
 
-        if timeout and not(0 <= timeout <= 4294967):
+        if timeout and not(0 <= timeout <= 4294967295):
             raise ValueError("timeout value is invalid")
 
         starting_time = time.clock()
 
         while True:
             if timeout is None:
-                adjusted_timeout = VI_TMO_INFINITE
+                adjusted_timeout = constants.VI_TMO_INFINITE
             else:
-                adjusted_timeout = int((starting_time + timeout - time.clock()) * 1000)
+                adjusted_timeout = int((starting_time + timeout - time.clock()))
                 if adjusted_timeout < 0:
                     adjusted_timeout = 0
 
-            event_type, context = lib.wait_on_event(self.session, VI_EVENT_SERVICE_REQ,
+            event_type, context = lib.wait_on_event(self.session, constants.VI_EVENT_SERVICE_REQ,
                                                     adjusted_timeout)
             lib.close(context)
             if self.stb & 0x40:
                 break
 
-        lib.discard_events(self.session, VI_EVENT_SERVICE_REQ, VI_QUEUE)
+        lib.discard_events(self.session, constants.VI_EVENT_SERVICE_REQ, constants.VI_QUEUE)
 
 
+@Resource.register(constants.InterfaceType.gpib, 'INTFC')
+class GPIBInterface(_GPIBMixin, Resource):
+    """Communicates with to devices of type GPIB::INTFC
+
+    More complex resource names can be specified with the following grammar:
+        GPIB[board]::INTFC
+
+    Do not instantiate directly, use :meth:`pyvisa.highlevel.ResourceManager.open`.
+
+    :param resource_manager: A resource manager instance.
+    :param resource_name: the VISA name for the resource (eg. "GPIB::10")
+    """
+
+    address_state = hlp.enum_attr('VI_ATTR_GPIB_ADDR_STATE', constants.AddressState,
+                                  'Shows whether the specified GPIB interface is currently addressed to talk or listen, or is not addressed.',
+                                  ro=True)
+
+    atn_state = hlp.enum_attr('VI_ATTR_GPIB_ATN_STATE', constants.LineState,
+                              'Shows the current state of the GPIB ATN (ATtentioN) interface line.',
+                              ro=True)
+
+    is_controller_in_charge = hlp.boolean_attr('VI_ATTR_GPIB_CIC_STATE',
+                                               'Shows whether the specified GPIB interface is currently CIC (Controller In Charge).',
+                                               ro=True)
+
+    is_system_controller = hlp.boolean_attr('VI_ATTR_GPIB_SYS_CNTRL_STATE',
+                                            'Shows whether the specified GPIB interface is currently the system controller.\n\n'
+                                            'In some implementations, this attribute may be modified only through a configuration utility.',
+                                            ro=True)
+    ndac_state = hlp.enum_attr('VI_ATTR_GPIB_NDAC_STATE', constants.LineState,
+                               'Shows the current state of the GPIB NDAC (Not Data ACcepted) interface line.',
+                               ro=True)
