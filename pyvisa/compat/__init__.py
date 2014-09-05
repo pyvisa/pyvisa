7459a005b96b74644616b84088db5e3b706ac53d
Hernan Grecco <hernan.grecco@gmail.com>
2014-09-05 17:48:34 -0300

    Added with_metaclass compatibility helper

--- pyvisa/compat/__init__.py
+++ pyvisa/compat/__init__.py
@@ -51,3 +51,13 @@ try:
 except ImportError:
     from .check_output import check_output
 
+
+def with_metaclass(meta, *bases):
+    """Create a base class with a metaclass."""
+    # This requires a bit of explanation: the basic idea is to make a dummy
+    # metaclass for one level of class instantiation that replaces itself with
+    # the actual metaclass.
+    class metaclass(meta):
+        def __new__(cls, name, this_bases, d):
+            return meta(name, bases, d)
+    return type.__new__(metaclass, str('temporary_class'), (), {})
