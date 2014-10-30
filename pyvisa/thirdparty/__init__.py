c5ac6458fc28a4e24a6fecbdc226835dbed9563b
Hernan Grecco <hernan.grecco@gmail.com>
2014-10-30 01:28:15 -0300

    Bundled PrettyTable with PyVISA

---
+++ pyvisa/thirdparty/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+    pyvisa.thirdparty
+    ~~~~~~~~~~~~~~~~~
+
+    Third party libraries and modules bundled with PyVISA.
+
+    This file is part of PyVISA.
+
+    :copyright: 2014 by PyVISA Authors, see AUTHORS for more details.
+    :license: MIT, see LICENSE for more details.
+"""
