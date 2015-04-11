c1c8d44b04d721bbe7d65825667f947685b32a37
Hernan Grecco <hernan.grecco@gmail.com>
2015-04-10 23:31:58 -0300

    Implements ResourceName class.
    Provides a pure Python parser for VISA resource names.

---
+++ pyvisa/rname.py
@@ -0,0 +1,368 @@
+# -*- coding: utf-8 -*-
+"""
+    pyvisa.rname
+    ~~~~~~~~~~~~
+
+    Functions and classes to parse and assemble resource name.
+
+    :copyright: 2014 by PyVISA Authors, see AUTHORS for more details.
+    :license: MIT, see LICENSE for more details.
+"""
+
+from __future__ import division, unicode_literals, print_function, absolute_import
+
+from collections import namedtuple, defaultdict
+
+from pyvisa import constants
+
+# :type: set[str]
+_INTERFACE_TYPES = set()
+
+# Resource Class for Interface type
+# :type: dict[str, set[str]]
+_RESOURCE_CLASSES = defaultdict(set)
+
+# :type: dict[(str, str), ResourceName]
+_SUBCLASSES = {}
+
+# DEFAULT Resource Class for a given interface type.
+# :type: dict[str, str]
+_DEFAULT_RC = {}
+
+
+class InvalidResourceName(ValueError):
+    """Exception raised when the resource name cannot be parsed.
+    """
+
+    def __init__(self, msg):
+        self.msg = msg
+
+    @classmethod
+    def bad_syntax(cls, syntax, resource_name, ex=None):
+        """Exception used when the resource name cannot be parsed.
+        """
+
+        if ex:
+            msg = "The syntax is '%s' (%s)." % (syntax, ex)
+        else:
+            msg = "The syntax is '%s'." % syntax
+
+        msg = "Could not parse '%s'. %s" % (resource_name, msg)
+
+        return cls(msg)
+
+    @classmethod
+    def subclass_notfound(cls, interface_type_resource_class, resource_name=None):
+        """Exception used when the subclass for a given interface type / resource class pair
+        cannot be found.
+        """
+
+        msg = "Parser for not found (%s)." % interface_type_resource_class
+
+        if resource_name:
+            msg = "Could not parse '%s'. %s" % (resource_name, msg)
+
+        return cls(msg)
+
+    @classmethod
+    def rc_notfound(cls, interface_type, resource_name=None):
+        """Exception used when no resource class is provided and no default is found.
+        """
+
+        msg = "Resource class for %s not provided and default not found." % interface_type
+
+        if resource_name:
+            msg = "Could not parse '%s'. %s" % (resource_name, msg)
+
+        return cls(msg)
+
+    def __str__(self):
+        return self.msg
+
+
+def register_subclass(cls):
+    """Register a subclass for a given interface type and resource class.
+    """
+
+    key = cls.interface_type, cls.resource_class
+
+    if key in _SUBCLASSES:
+        raise ValueError('Class already registered for %s and %s' % key)
+
+    _SUBCLASSES[(cls.interface_type, cls.resource_class)] = cls
+
+    _INTERFACE_TYPES.add(cls.interface_type)
+    _RESOURCE_CLASSES[cls.interface_type].add(cls.resource_class)
+
+    if cls.is_rc_optional:
+        if cls.interface_type in _DEFAULT_RC:
+            raise ValueError('Default already specified for %s' %
+                             cls.interface_type)
+        _DEFAULT_RC[cls.interface_type] = cls.resource_class
+
+    return cls
+
+
+class ResourceName(object):
+    """Base class for ResourceNames to be used as a Mixin
+    """
+
+    # Interface type string
+    interface_type = ''
+
+    # Resource class string
+    resource_class = ''
+
+    # Specifices if the resource class part of the string is optional.
+    is_rc_optional = False
+
+    # Formatting string for canonical
+    _canonical_fmt = ''
+
+    # VISA syntax for resource
+    _visa_syntax = ''
+
+    # Resource name provided by the user (not empty only when parsing)
+    user = ''
+
+    @classmethod
+    def from_string(cls, resource_name):
+        """Parse a resource name and return a ResourceName
+
+        :type resource_name: str
+        :rtype: ResourceName
+
+        :raises InvalidResourceName: if the resource name is invalid.
+        """
+        # TODO Remote VISA
+
+        uname = resource_name.upper()
+
+        for interface_type in _INTERFACE_TYPES:
+
+            # Loop through all known interface types until we found one
+            # that matches the beginning of the resource name
+            if not uname.startswith(interface_type):
+                continue
+
+            if len(resource_name) == len(interface_type):
+                parts = ()
+            else:
+                parts = resource_name[len(interface_type):].split('::')
+
+            # Try to match the last part of the resource name to
+            # one of the known resource classes for the given interface type.
+            # If not possible, use the default resource class
+            # for the given interface type.
+            if parts and parts[-1] in _RESOURCE_CLASSES[interface_type]:
+                parts, resource_class = parts[:-1], parts[-1]
+            else:
+                try:
+                    resource_class = _DEFAULT_RC[interface_type]
+                except KeyError:
+                    raise InvalidResourceName.rc_notfound(interface_type, resource_name)
+
+            # Look for the subclass
+            try:
+                subclass = _SUBCLASSES[(interface_type, resource_class)]
+            except KeyError:
+                raise InvalidResourceName.subclass_notfound((interface_type, resource_class), resource_name)
+
+            # And create the object
+            try:
+                rn = subclass.from_parts(*parts)
+                rn.user = resource_name
+                return rn
+            except ValueError as ex:
+                raise InvalidResourceName.bad_syntax(subclass.syntax, resource_name, ex)
+
+        raise InvalidResourceName('Could not parse %s: unknown interface type' % resource_name)
+
+    @classmethod
+    def from_kwargs(cls, **kwargs):
+        interface_type = kwargs.pop('interface_type')
+
+        try:
+            resource_class = kwargs.pop('resource_class', _DEFAULT_RC[interface_type])
+        except KeyError:
+            raise InvalidResourceName.rc_notfound(interface_type)
+
+        # Look for the subclass
+        try:
+            subclass = _SUBCLASSES[(interface_type, resource_class)]
+        except KeyError:
+            raise InvalidResourceName.subclass_notfound((interface_type, resource_class))
+
+        # And create the object
+        try:
+            return subclass(**kwargs)
+        except ValueError as ex:
+            raise InvalidResourceName(str(ex))
+
+    def __str__(self):
+        return self._canonical_fmt.format(self)
+
+
+def build_rn_class(interface_type, resource_parts, resource_class,
+                   is_rc_optional=True):
+    """Builds a resource name class by mixing a named tuple and ResourceName.
+
+    It also registers the class.
+
+    :param interface_type: the interface type
+    :type: interface_type: str
+    :param resource_parts: each of the parts of the resource name indicating
+                           name and default value. Use None for mandatory
+                           fields.
+    :type resource_parts: tuple[(str, str)]
+    :param resource_class: the resource class
+    :type resource_class: str
+    :param is_rc_optional: indicates if the resource class part is optional
+    :type is_rc_optional: boolean.
+    """
+
+    interface_type = interface_type.upper()
+    resource_class = resource_class.upper()
+
+    syntax = interface_type
+    fmt = interface_type
+    fields = []
+
+    # Contains the resource parts but using python friendly names
+    # (all lower case and replacing spaces by underscores)
+    p_resource_parts = []
+
+    kwdoc = []
+
+    # Assemble the syntax and format string based on the resource parts
+    for ndx, (name, default_value) in enumerate(resource_parts):
+        pname = name.lower().replace(' ', '_')
+        fields.append(pname)
+        p_resource_parts.append((pname, default_value))
+
+        sep = '::' if ndx else ''
+
+        fmt += sep + '{0.%s}' % pname
+
+        if default_value is None:
+            syntax += sep + name
+        else:
+            syntax += '[' + sep + name + ']'
+
+        kwdoc.append('- %s (%s)' % (pname, 'required' if default_value is None
+                                    else default_value))
+
+    fmt += '::' + resource_class
+
+    if not is_rc_optional:
+        syntax += '::' + resource_class
+    else:
+        syntax += '[' + '::' + resource_class + ']'
+
+    class _C(namedtuple('Internal', ' '.join(fields)), ResourceName):
+        """%s %s"
+
+        Can be created with the following keyword only arguments:
+            %s
+
+        Format :
+            %s
+        """ % (resource_class, interface_type, '    \n'.join(kwdoc), syntax)
+
+        def __new__(cls, **kwargs):
+            new_kwargs = dict(p_resource_parts, **kwargs)
+
+            for key, value in new_kwargs.items():
+                if value is None:
+                    raise ValueError(key + ' is a required parameter')
+
+            return super(_C, cls).__new__(cls, **new_kwargs)
+
+        @classmethod
+        def from_parts(cls, *parts):
+
+            if len(parts) < sum(1 for _, v in p_resource_parts
+                                if v is not None):
+                raise ValueError('not enough parts')
+            elif len(parts) > len(p_resource_parts):
+                raise ValueError('too many parts')
+
+            (k, default), rp = p_resource_parts[0], p_resource_parts[1:]
+
+            # The first part (just after the interface_type) is the only
+            # optional part which can be and empty and therefore the
+            # default value should be used.
+            p, pending = parts[0], parts[1:]
+            kwargs = {k: default if p == '' else p}
+
+            # The rest of the parts are consumed when mandatory elements are required.
+            while len(pending) < len(rp):
+                (k, default), rp = rp[0], rp[1:]
+                if default is None:
+                    if not parts:
+                        raise ValueError(k + ' part is mandatory')
+                    p, pending = pending[0], pending[1:]
+                    if not p:
+                        raise ValueError(k + ' part is mandatory')
+                    kwargs[k] = p
+                else:
+                    kwargs[k] = default
+
+            # When the length of the pending provided and resource parts
+            # are equal, we just consume everything.
+            kwargs.update((k, p) for (k, v), p in zip(rp, pending))
+
+            return cls(**kwargs)
+
+    _C.interface_type = interface_type
+    _C.resource_class = resource_class
+    _C.is_rc_optional = is_rc_optional
+    _C._canonical_fmt = fmt
+    _C._visa_syntax = syntax
+
+    _C.__name__ = str(interface_type + resource_class.title())
+
+    return register_subclass(_C)
+
+# Build subclasses for each resource
+
+GPIBInstr = build_rn_class('GPIB',
+                           (('board', '0'), ('primary address', None),
+                            ('secondary address', constants.VI_NO_SEC_ADDR)),
+                           'INSTR')
+
+GPIBIntfc = build_rn_class('GPIB', (('board', '0'), ), 'INTFC', False)
+
+ASRLInstr = build_rn_class('ASRL', (('board', '0'), ), 'INSTR')
+
+TCPIPInstr = build_rn_class('TCPIP', (('board', '0'), ('host address', None),
+                                      ('LAN device name', 'inst0'), ), 'INSTR')
+
+TCPIPSocket = build_rn_class('TCPIP', (('board', '0'), ('host address', None),
+                                       ('port', None), ), 'SOCKET', False)
+
+USBInstr = build_rn_class('USB',
+                          (('board', '0'), ('manufacturer ID', None),
+                           ('model code', None), ('serial number', None),
+                           ('USB interface number', '0')), 'INSTR')
+
+USBRaw = build_rn_class('USB', (('board', '0'), ('manufacturer ID', None),
+                                ('model code', None), ('serial number', None),
+                                ('USB interface number', '0')), 'RAW', False)
+
+
+def assemble_canonical_name(**kwargs):
+    return str(ResourceName.from_kwargs(**kwargs))
+
+
+def to_canonical_name(resource_name):
+    """Parse a resource name and return the canonical version.
+
+    :type resource_name: str
+    :rtype: str
+    """
+    return str(ResourceName.from_string(resource_name))
+
+
+parse_resource_name = ResourceName.from_string
+
---
+++ pyvisa/testsuite/test_rname.py
@@ -0,0 +1,229 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import division, unicode_literals, print_function, absolute_import
+
+from pyvisa.compat import unittest
+from pyvisa.testsuite import BaseTestCase
+
+from pyvisa import rname
+
+parse = rname.ResourceName.from_string
+
+
+class TestParsers(BaseTestCase):
+
+    def _parse_test(self, rn, **kwargs):
+        p = parse(rn)
+        r = {k: getattr(p, k) for k in p._fields + ('interface_type', 'resource_class')}
+        r['canonical_resource_name'] = str(p)
+        self.assertEqual(r, kwargs, rn)
+
+    @unittest.expectedFailure
+    def test_asrl_ethernet(self):
+        self._parse_test('ASRL::1.2.3.4::2::INSTR',
+                         interface_type='ASRL',
+                         resource_class='INSTR',
+                         port='2',
+                         address='1.2.3.4')
+
+    def test_asrl(self):
+        self._parse_test('ASRL1::INSTR',
+                         interface_type='ASRL',
+                         resource_class='INSTR',
+                         board='1',
+                         canonical_resource_name='ASRL1::INSTR')
+
+        self._parse_test('ASRL1',
+                         interface_type='ASRL',
+                         resource_class='INSTR',
+                         board='1',
+                         canonical_resource_name='ASRL1::INSTR')
+
+    def test_gpib_instr(self):
+        self._parse_test('GPIB::1::0::INSTR',
+                         interface_type='GPIB',
+                         resource_class='INSTR',
+                         board='0',
+                         primary_address='1',
+                         secondary_address='0',
+                         canonical_resource_name='GPIB0::1::0::INSTR')
+
+        self._parse_test('GPIB::1::INSTR',
+                         interface_type='GPIB',
+                         resource_class='INSTR',
+                         board='0',
+                         primary_address='1',
+                         secondary_address=65535,
+                         canonical_resource_name='GPIB0::1::65535::INSTR')
+
+        self._parse_test('GPIB1::1::INSTR',
+                         interface_type='GPIB',
+                         resource_class='INSTR',
+                         board='1',
+                         primary_address='1',
+                         secondary_address=65535,
+                         canonical_resource_name='GPIB1::1::65535::INSTR')
+
+        self._parse_test('GPIB1::1',
+                         interface_type='GPIB',
+                         resource_class='INSTR',
+                         board='1',
+                         primary_address='1',
+                         secondary_address=65535,
+                         canonical_resource_name='GPIB1::1::65535::INSTR')
+
+    def test_gpib_intf(self):
+        self._parse_test('GPIB::INTFC',
+                         interface_type='GPIB',
+                         resource_class='INTFC',
+                         board='0',
+                         canonical_resource_name='GPIB0::INTFC')
+
+        self._parse_test('GPIB3::INTFC',
+                         interface_type='GPIB',
+                         resource_class='INTFC',
+                         board='3',
+                         canonical_resource_name='GPIB3::INTFC')
+
+    def test_tcpip_intr(self):
+
+        self._parse_test('TCPIP::192.168.134.102',
+                         interface_type='TCPIP',
+                         resource_class='INSTR',
+                         host_address='192.168.134.102',
+                         board='0',
+                         lan_device_name='inst0',
+                         canonical_resource_name='TCPIP0::192.168.134.102::inst0::INSTR')
+
+        self._parse_test('TCPIP::dev.company.com::INSTR',
+                         interface_type='TCPIP',
+                         resource_class='INSTR',
+                         host_address='dev.company.com',
+                         board='0',
+                         lan_device_name='inst0',
+                         canonical_resource_name='TCPIP0::dev.company.com::inst0::INSTR')
+
+        self._parse_test('TCPIP3::dev.company.com::inst3::INSTR',
+                         interface_type='TCPIP',
+                         resource_class='INSTR',
+                         host_address='dev.company.com',
+                         board='3',
+                         lan_device_name='inst3',
+                         canonical_resource_name='TCPIP3::dev.company.com::inst3::INSTR')
+
+        self._parse_test('TCPIP3::1.2.3.4::inst3::INSTR',
+                         interface_type='TCPIP',
+                         resource_class='INSTR',
+                         host_address='1.2.3.4',
+                         board='3',
+                         lan_device_name='inst3',
+                         canonical_resource_name='TCPIP3::1.2.3.4::inst3::INSTR')
+
+    def test_tcpip_socket(self):
+        self._parse_test('TCPIP::1.2.3.4::999::SOCKET',
+                         interface_type='TCPIP',
+                         resource_class='SOCKET',
+                         host_address='1.2.3.4',
+                         board='0',
+                         port='999',
+                         canonical_resource_name='TCPIP0::1.2.3.4::999::SOCKET')
+
+        self._parse_test('TCPIP2::1.2.3.4::999::SOCKET',
+                         interface_type='TCPIP',
+                         resource_class='SOCKET',
+                         host_address='1.2.3.4',
+                         board='2',
+                         port='999',
+                         canonical_resource_name='TCPIP2::1.2.3.4::999::SOCKET')
+
+    def test_usb_instr(self):
+        self._parse_test('USB::0x1234::125::A22-5::INSTR',
+                         interface_type='USB',
+                         resource_class='INSTR',
+                         manufacturer_id='0x1234',
+                         model_code='125',
+                         serial_number='A22-5',
+                         board='0',
+                         usb_interface_number='0',
+                         canonical_resource_name='USB0::0x1234::125::A22-5::0::INSTR')
+
+        self._parse_test('USB2::0x1234::125::A22-5::INSTR',
+                         interface_type='USB',
+                         resource_class='INSTR',
+                         manufacturer_id='0x1234',
+                         model_code='125',
+                         serial_number='A22-5',
+                         board='2',
+                         usb_interface_number='0',
+                         canonical_resource_name='USB2::0x1234::125::A22-5::0::INSTR')
+
+        self._parse_test('USB::0x1234::125::A22-5',
+                         interface_type='USB',
+                         resource_class='INSTR',
+                         manufacturer_id='0x1234',
+                         model_code='125',
+                         serial_number='A22-5',
+                         board='0',
+                         usb_interface_number='0',
+                         canonical_resource_name='USB0::0x1234::125::A22-5::0::INSTR')
+
+        self._parse_test('USB::0x1234::125::A22-5::3::INSTR',
+                         interface_type='USB',
+                         resource_class='INSTR',
+                         manufacturer_id='0x1234',
+                         model_code='125',
+                         serial_number='A22-5',
+                         board='0',
+                         usb_interface_number='3',
+                         canonical_resource_name='USB0::0x1234::125::A22-5::3::INSTR')
+
+        self._parse_test('USB2::0x1234::125::A22-5::3::INSTR',
+                         interface_type='USB',
+                         resource_class='INSTR',
+                         manufacturer_id='0x1234',
+                         model_code='125',
+                         serial_number='A22-5',
+                         board='2',
+                         usb_interface_number='3',
+                         canonical_resource_name='USB2::0x1234::125::A22-5::3::INSTR')
+
+        self._parse_test('USB1::0x1234::125::A22-5::3',
+                         interface_type='USB',
+                         resource_class='INSTR',
+                         manufacturer_id='0x1234',
+                         model_code='125',
+                         serial_number='A22-5',
+                         board='1',
+                         usb_interface_number='3',
+                         canonical_resource_name='USB1::0x1234::125::A22-5::3::INSTR')
+
+    def test_usb_raw(self):
+        self._parse_test('USB::0x1234::125::A22-5::RAW',
+                         interface_type='USB',
+                         resource_class='RAW',
+                         manufacturer_id='0x1234',
+                         model_code='125',
+                         serial_number='A22-5',
+                         board='0',
+                         usb_interface_number='0',
+                         canonical_resource_name='USB0::0x1234::125::A22-5::0::RAW')
+
+        self._parse_test('USB2::0x1234::125::A22-5::RAW',
+                         interface_type='USB',
+                         resource_class='RAW',
+                         manufacturer_id='0x1234',
+                         model_code='125',
+                         serial_number='A22-5',
+                         board='2',
+                         usb_interface_number='0',
+                         canonical_resource_name='USB2::0x1234::125::A22-5::0::RAW')
+
+        self._parse_test('USB2::0x1234::125::A22-5::3::RAW',
+                         interface_type='USB',
+                         resource_class='RAW',
+                         manufacturer_id='0x1234',
+                         model_code='125',
+                         serial_number='A22-5',
+                         board='2',
+                         usb_interface_number='3',
+                         canonical_resource_name='USB2::0x1234::125::A22-5::3::RAW')
