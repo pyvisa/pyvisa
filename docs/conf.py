dda9c69974fa89904000bf815ff1356dab4d4fee
MatthieuDartiailh <marul@laposte.net>
2018-09-06 16:40:12 -0400

    docs: use the plain readthedocs theme

--- docs/conf.py
+++ docs/conf.py
@@ -102,7 +102,6 @@ html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
-html_theme_path = ['_themes']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
@@ -135,11 +134,6 @@ html_static_path = ['_static']
 
 # Custom sidebar templates, maps document names to template names.
 #html_sidebars = {}
-html_sidebars = {
-    'index':    ['sidebarintro.html', 'sourcelink.html', 'searchbox.html'],
-    '**':       ['sidebarlogo.html', 'localtoc.html', 'relations.html',
-                 'sourcelink.html', 'searchbox.html']
-}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
