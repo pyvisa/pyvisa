885f541971aecf4fe65db83d0f7c269f3991a30e
MatthieuDartiailh <marul@laposte.net>
2019-10-06 22:26:30 -0400

    testsuite: add files for new tests

---
+++ pyvisa/testsuite/keysight_assisted_tests/test_util.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""Test pyvisa utility functions that requires a VISA library to be tested.
+
+"""
+from pyvisa.testsuite import BaseTestCase
+
+
+# XXX system details and binary analysis
+class TestSystemDetailsAnalysis(BaseTestCase):
+    """
+
+    """
